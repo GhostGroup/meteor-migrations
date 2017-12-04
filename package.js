c2050e474a1bb65ef327d7d2781a0d91943a829f
David Fudge <rkstar@mac.com>
2017-12-04 06:28:12 -0500

    refactoring and directory structure organization

--- package.js
+++ package.js
@@ -1,20 +1,19 @@
 Package.describe({
   summary: 'Define and run db migrations.',
-  version: '1.0.2',
-  name: 'percolate:migrations',
-  git: 'https://github.com/percolatestudio/meteor-migrations.git',
+  version: '1.1.0',
+  name: 'growone:migrations',
+  git: 'https://github.com/GhostGroup/meteor-migrations.git',
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@1.5');
-  api.use('ecmascript');
+  api.use(['ecmascript', 'modules']);
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
-  api.addFiles(['migrations_server.js'], 'server');
-  api.export('Migrations', 'server');
+  api.mainModule('src/server/index.js', 'server');
 });
 
 Package.on_test(function(api) {
-  api.use('ecmascript');
+  api.use(['ecmascript', 'modules']);
   api.use(['percolate:migrations', 'tinytest']);
-  api.addFiles('migrations_tests.js', ['server']);
+  api.addFiles('tests/index.js', ['server']);
 });
